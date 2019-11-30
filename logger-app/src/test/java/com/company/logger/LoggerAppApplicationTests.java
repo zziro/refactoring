bda0756f06e50fa6900b58987e95c06f51468866
zziro <zziro.123@gmail.com>
2019-11-30 12:16:58 -0500

    basic configuration

---
+++ logger-app/src/test/java/com/company/logger/LoggerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.logger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
